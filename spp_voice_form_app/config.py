17b8eec3145710b51a84a03de1ca4285752fc2e4
gusarov2906 <gusarov2906@gmail.com>
2022-10-23 11:04:40 +0300

    Fix model Speed up

---
+++ spp_voice_form_app/config.py
@@ -0,0 +1,4 @@
+from .stt import STT
+from .tts import TTS
+stt_ = STT()
+tts_ = TTS()
--- spp_voice_form_app/views.py
+++ spp_voice_form_app/views.py
@@ -3,28 +3,30 @@ from django.http import HttpResponse
 from .stt import STT
 from .tts import TTS
 from text_to_num import alpha2digit
+from .config import tts_
+from .config import stt_
 
-
+    
 def index(request):
     return render(request, 'index.html')
 
 def voice_request(request):
+    global stt_
     name = './file_sound.wav'
     text = 'none'
     #print(request.body)
     f = open(name, 'wb')
     f.write(request.body)
     f.close()
-    stt_ = STT()
     text = stt_.audio_to_text(name)
     text = alpha2digit(text, 'ru')
     return HttpResponse(text, content_type="text/plain")
 
 def text_request(request):
+    global tts_
     name = './file_text.wav'
     text = request.body.decode("utf-8")
     print(text)
-    tts_ = TTS()
     tts_.text_to_wav(text, name)
     f = open(name, 'rb')
     data = f.read()
